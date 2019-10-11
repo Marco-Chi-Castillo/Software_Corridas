4f2b6d73a4f65b22cfea30d8133d887959479d43
Marco Antonio Chi Castillo <chimarcoantonio@gmail.com>
2019-10-10 23:40:03 -0500

    Inicio de Proyecto

---
+++ src/software_corridas/Software_Corridas.java
@@ -0,0 +1,15 @@
+
+package software_corridas;
+
+/**
+ *
+ * @author marco antonio chi castillo 
+ * @author marcos abraham caamal tzuc
+ */
+public class Software_Corridas {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
