e194228669920ac058afbb5f1435a410996d8297
MarcosCaamal <m04caamal@gmail.com>
2019-10-12 10:38:23 -0500

    cambios 2

--- src/software_corridas/Software_Corridas.java
+++ src/software_corridas/Software_Corridas.java
@@ -17,19 +17,9 @@ import java.util.ArrayList;
 public class Software_Corridas {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        CargarArchivo prueba=new CargarArchivo();
-        ArrayList <String> texto=new ArrayList();
-        texto=prueba.leer_fichero();
-        for (int i=0; i<texto.size(); i++){
-            System.out.println(texto.get(i));
-        }
-    
-=======
         frmInterfaz frm = new frmInterfaz();
         frm.setVisible(true);
         frm.setSize(550, 350);
->>>>>>> 7003fc1a23e35e20d8e174958b1bfeae0561eacf
     }
     
 }
