7003fc1a23e35e20d8e174958b1bfeae0561eacf
Marco Antonio Chi Castillo <chimarcoantonio@gmail.com>
2019-10-11 23:07:16 -0500

    Se agrega Interfaz de Usuario

--- src/software_corridas/Software_Corridas.java
+++ src/software_corridas/Software_Corridas.java
@@ -2,14 +2,15 @@
 package software_corridas;
 
 /**
- *
  * @author marco antonio chi castillo 
  * @author marcos abraham caamal tzuc
  */
 public class Software_Corridas {
 
     public static void main(String[] args) {
-        
+        frmInterfaz frm = new frmInterfaz();
+        frm.setVisible(true);
+        frm.setSize(550, 350);
     }
     
 }
